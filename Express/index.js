635c1a6390c364aaf4dafb4ff3e8d7606167d05e
vinay980 <vinaypatel980pp@gmail.com>
2025-04-22 17:22:55 +0530

    add

--- Express/index.js
+++ Express/index.js
@@ -1,3 +1,5 @@
+import logger from "./logger.js"
+import morgan from "morgan"
 // const express = require("express")
 import express from "express"
 import "dotenv/config"
@@ -5,10 +7,28 @@ import "dotenv/config"
 const app = express()
 const port = process.env.PORT || 9000
 
-app.get("/",(req,res)=>{
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+    morgan(morganFormat, {
+        stream: {
+            write: (message) => {
+                const logObject = {
+                    method: message.split(" ")[0],
+                    url: message.split(" ")[1],
+                    status: message.split(" ")[2],
+                    responseTime: message.split(" ")[3],
+                };
+                logger.info(JSON.stringify(logObject));
+            },
+        },
+    })
+);
+
+app.get("/", (req, res) => {
     res.end("Hello1")
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server start at localhost:${port}`);
 })
